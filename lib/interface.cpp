c0c55dc62c96601b47cb1ea618f950a9cb04c7c7
lpp_gitee <lipanpan1@uniontech.com>
2022-07-19 17:43:58 +0800

    come true string2time function for interface.cpp

--- lib/interface.cpp
+++ lib/interface.cpp
@@ -590,7 +590,17 @@ void time2string(const time_t time, string& des) {
 }
 
 void string2time(time_t& time, const string& des) {
-    return;
+    tm tm_;                                    // 定义tm结构体。
+    int year, month, day, hour, minute, second;// 定义时间的各个int临时变量。
+    sscanf(des.c_str(), "%d-%d-%d %d:%d:%d", &year, &month, &day, &hour, &minute, &second);// 将string存储的日期时间，转换为int临时变量。
+    tm_.tm_year = year - 1900;                 // 年，由于tm结构体存储的是从1900年开始的时间，所以tm_year为int临时变量减去1900。
+    tm_.tm_mon = month - 1;                    // 月，由于tm结构体的月份存储范围为0-11，所以tm_mon为int临时变量减去1。
+    tm_.tm_mday = day;                         // 日。
+    tm_.tm_hour = hour;                        // 时。
+    tm_.tm_min = minute;                       // 分。
+    tm_.tm_sec = second;                       // 秒。
+    tm_.tm_isdst = 0;                          // 非夏令时。
+    time = mktime(&tm_);                       // 将tm结构体转换成time_t格式。
 }
 
 }
