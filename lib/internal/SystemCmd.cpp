3885989357ae0d97f09c03ca3e85e0a1fc549cec
zhangkea <zhangkea@uniontech.com>
2022-07-20 11:30:00 +0800

    come true invalidate function

--- lib/internal/SystemCmd.cpp
+++ lib/internal/SystemCmd.cpp
@@ -349,6 +349,11 @@ SystemCmd::getLine( unsigned Nr_iv, bool Sel_bv, OutputStream Idx_iv ) const
 void
 SystemCmd::invalidate()
 {
-	return;
+    for (int Idx_ii = 0; Idx_ii < 2; Idx_ii++)
+    {
+	SelLines_aC[Idx_ii].resize(0);
+	Lines_aC[Idx_ii].clear();
+	NewLineSeen_ab[Idx_ii] = true;
+    }
 }
 
